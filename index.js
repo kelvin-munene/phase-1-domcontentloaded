322cf3f47fb628828e3ceab2185908a8e22be41d
kevin <kmunene858@gmail.com>
2022-06-15 10:39:12 +0300

    commit tree 9c486e48702fc53ecfd8f73744f99f6f6ece600c

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM loaded")
+    modifyText( )
+
+});
+
+
+
+function modifyText( ){
+    const text = document.getElementById("text")
+    text.textContent = "This is really cool!"
+}
